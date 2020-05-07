6fda31f77f4ad82182f923b6375c2410d871dd62
space47 <varnitgupta47@gmial.com>
2020-05-07 12:09:17 +0530

    commited

---
+++ script1.js
@@ -0,0 +1,21 @@
+(function () {
+	var names=["Yaakov", "John", "Jen", "Jason",
+	"Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+
+	for (var i=0; i< names.length; i++ )
+	{
+		var firstletter=names[i].charAt(0);
+
+		if( firstletter==='j' || firstletter==='J')
+		{
+			console.log("Good Bye "+names[i]);
+		}
+		else
+		{
+			
+			console.log("Hello "+names[i]);
+		}
+	}
+  
+})();
